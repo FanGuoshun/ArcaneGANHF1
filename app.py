71753e8244b9133080ed1214e698c08ff7319ea5
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-12-09 07:04:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from facenet_pytorch import MTCNN
 from torchvision import transforms
 import torch, PIL
 torch.hub.download_url_to_file('https://www.giantfreakinrobot.com/wp-content/uploads/2021/02/adult-groot.jpg', 'groot.jpg')
+torch.hub.download_url_to_file('https://hf.space/gradioiframe/akhaliq/AnimeGANv2/file/bill.png', 'bill.png')
 
 from tqdm.notebook import tqdm
 import gradio as gr
@@ -148,6 +149,6 @@ gr.Interface(
     title=title,
     description=description,
     article=article,
-    examples=[['groot.jpg']],
+    examples=[['groot.jpg'],['bill.png']],
     enable_queue=True
    ).launch(debug=True)
