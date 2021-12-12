6c313c40fe1c325912108308b2f810b75f8faf9a
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-12-12 16:27:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def process(im):
         
 title = "ArcaneGAN"
 description = "Gradio demo for ArcaneGAN, portrait to Arcane style. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
-article = "<p style='text-align: center'>ArcaneGan by <a href='https://twitter.com/devdef' target='_blank'>Alexander S</a> | <a href='https://github.com/Sxela/ArcaneGAN' target='_blank'>Github Repo</a></p>"
+article = "<p style='text-align: center'>ArcaneGan by <a href='https://twitter.com/devdef' target='_blank'>Alexander S</a> | <a href='https://github.com/Sxela/ArcaneGAN' target='_blank'>Github Repo</a> | ![visitor badge](https://visitor-badge.glitch.me/badge?page_id=jwenjian.visitor-badge)</p>"
 
 gr.Interface(
     process, 
