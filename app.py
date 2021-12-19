79b688e9b32722c22ba2a14c5c28028c5ebe3527
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-12-19 15:35:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from facenet_pytorch import MTCNN
 from torchvision import transforms
 import torch, PIL
 torch.hub.download_url_to_file('https://hf.space/gradioiframe/akhaliq/AnimeGANv2/file/bill.png', 'bill.png')
-
 from tqdm.notebook import tqdm
 import gradio as gr
 
