5b9db09e470612cfcb619ea1bc5c24ee899119a9
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-12-30 21:17:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pystuck
 pystuck.run_server()
 
 import os
-os.system("pip freeze")
 os.system("wget https://github.com/Sxela/ArcaneGAN/releases/download/v0.4/ArcaneGANv0.4.jit")
 os.system("wget https://github.com/Sxela/ArcaneGAN/releases/download/v0.3/ArcaneGANv0.3.jit")
 os.system("wget https://github.com/Sxela/ArcaneGAN/releases/download/v0.2/ArcaneGANv0.2.jit")
