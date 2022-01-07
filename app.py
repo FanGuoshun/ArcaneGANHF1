15c085b1d97605637f6dafcf99a3da60db6427e4
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2022-01-07 22:18:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pystuck
 pystuck.run_server()
 
 import os
-
 os.system("pip install gradio==2.4.6")
 os.system("wget https://github.com/Sxela/ArcaneGAN/releases/download/v0.4/ArcaneGANv0.4.jit")
 os.system("wget https://github.com/Sxela/ArcaneGAN/releases/download/v0.3/ArcaneGANv0.3.jit")
